b16f983f92124add410c5a823c765571ed6dfeed
ankoval <an.koval@svitla.com>
2020-02-28 21:54:50 +0200

    es to arango exclude existed

--- migrate.py
+++ migrate.py
@@ -9,20 +9,21 @@ def main():
     assert ARANGO_ROOT_PASSWORD
     assert ARANGO_COLLECTION
     assert ARANGO_USERNAME
+
     es = Elasticsearch([ES_HOST])
     conn = Connection(username=ARANGO_USERNAME, password=ARANGO_ROOT_PASSWORD)
+
     if ES_INDEX not in conn.databases:
         conn.createDatabase(name=ES_INDEX)
     db = conn[ES_INDEX]
+
     if not db.hasCollection(ARANGO_COLLECTION):
         db.createCollection(name=ARANGO_COLLECTION)
 
-    # existed_patents_files = [
-    # _file key for _file in db.AQLQuery(f"FOR doc IN {ARANGO_COLLECTION} RETURN doc", rawResults=True)
-    # ]
-    # import ipdb; ipdb.set_trace()
+    existed_patents = db.AQLQuery(f"FOR doc IN {ARANGO_COLLECTION} RETURN doc._file").response['result']
+    es_query_exclude_existed = {"query": {"bool": {"must_not": [{"ids": {"values": existed_patents}}]}}}
+    res = es.search(index=ES_INDEX, body=es_query_exclude_existed)
 
-    res = es.search(index=ES_INDEX, body={"query": {"match_all": {}}})
     aql_query_insert = f"INSERT @doc INTO {ARANGO_COLLECTION} LET newDoc = NEW RETURN newDoc"
     for hit in res['hits']['hits']:
         hit['_file'] = hit['_id']
